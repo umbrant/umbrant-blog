4a4b12c2e2abe881b350148d19cb9d6ef34d19f6
Andrew Wang <andrew@umbrant.com>
2011-04-01 11:42:51 -0700

    Move tracking code. No bottom-border on last post on index.html

--- deploy/media/js/umbrant.js
+++ deploy/media/js/umbrant.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
 	$("#sidebar ul li ul li:first-child").addClass("first");
 	$("#sidebar #menu ul li:first-child").addClass("first");
+	$("#content .post:last-child .entry").css("border-bottom", "0");
 });
--- media/js/umbrant.js
+++ media/js/umbrant.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
+	// Apply special "first" class to sidebar sections, for spacing
 	$("#sidebar ul li ul li:first-child").addClass("first");
 	$("#sidebar #menu ul li:first-child").addClass("first");
+	// Remove border from last post in index listing
+	$("#content .post:last-child .entry").css("border-bottom", "0");
 });
